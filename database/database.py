9bae24461b4f173221e95f691ad1657030436c06
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-15 21:58:05 +0330

    feat(database-database): create engine

---
+++ database/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine
+
+
+engine = create_engine(
+    "sqlite:///database.db",
+    connect_args={"check_same_thread": False}
+)
