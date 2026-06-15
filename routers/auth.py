3c13aa173503f3e5a5d22ddb9dc9a542c7a86daf
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-16 02:14:31 +0330

    feat(routers-auth): add register route

---
+++ routers/auth.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.routing import APIRouter
+from sqlalchemy.orm import Session
+from database.schema import UserCreateSchema, UserResponseSchema
+from database.database import get_db
+from database.models import User
+
+router = APIRouter()
+
+
+@router.post("/register", response_model=UserResponseSchema)
+def register(request: UserCreateSchema, db: Session = Depends(get_db)):
+    existing_user = (
+        db.query(User)
+        .filter_by(username=request.username, password_hash=request.password)
+        .first()
+    )
+
+    if not existing_user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    return existing_user
