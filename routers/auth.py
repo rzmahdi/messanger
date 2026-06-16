f84017685d0e882e8c9e63644842d9d99413a224
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-17 02:52:42 +0330

    feat(auth): add login route

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,10 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
-from database.schema import UserCreateSchema, UserResponseSchema
+from database.schema import UserCreateSchema, UserResponseSchema, UserLoginSchema
 from database.database import get_db
 from database.models import User
-from services.auth_service import hash_password
+from services.auth_service import hash_password, verify_password
 
 router = APIRouter()
 
@@ -25,3 +25,20 @@ def register(request: UserCreateSchema, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@router.post("/login", response_model=UserResponseSchema)
+def login(request: UserLoginSchema, db: Session=Depends(get_db)):
+    existing_user = (
+        db.query(User)
+        .filter_by(username=request.username)
+        .first()
+    )
+
+    if not existing_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="username or password are wrong!")
+    
+    if not verify_password(existing_user.password_hash, request.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="username or password are wrong!")
+
+    return existing_user
