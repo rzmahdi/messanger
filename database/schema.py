2325e52d7da38c8ab45143967d298373725f68a2
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-17 02:00:42 +0330

    fix(database-schema): show id just in response

--- database/schema.py
+++ database/schema.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class UserBaseSchema(BaseModel):
-    id: int
     username: str
 
 class UserCreateSchema(UserBaseSchema):
     password: str
 
 class UserResponseSchema(UserBaseSchema):
+    id: int
     created_at: datetime
