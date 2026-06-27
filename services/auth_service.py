0b56633544fcecdc41d09b17f944f519804011a8
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-27 13:06:42 +0330

    feat(service-auth): add access token generator

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,7 +1,12 @@
 from pwdlib import PasswordHash
 from database.models import User
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 
 password_hash = PasswordHash.recommended()
+SECRET_KEY = "87sadf230bjwe27240iud23973ne3ui23fg486hdf23h389ye39gd902jv52d6dm289"
+ALGORITHM = "HS256"
+
 
 def hash_password(password: str) -> str:
     return password_hash.hash(password)
@@ -19,3 +24,8 @@ def authenticate_user(username: str, password: str, db):
         return None
     
     return user
+
+
+def create_access_token(username: str, user_id: int, exp_delta: timedelta):
+    encode = {"sub": username, "id": user_id, "exp": datetime.now() + exp_delta}
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
