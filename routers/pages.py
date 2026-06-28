12574d2e3a725b5e7541730552927b760995daeb
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-28 15:14:16 +0330

    feat(pages): include static files

--- routers/pages.py
+++ routers/pages.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
+router.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @router.get("/register")
