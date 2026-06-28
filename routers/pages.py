88f4c954096ff89ed16834797727b11af9891bd3
MahdiRz <mahdirazmkhah45@gmail.com>
2026-06-28 15:03:53 +0330

    feat(pages): add register template

--- routers/pages.py
+++ routers/pages.py
@@ -1,5 +1,13 @@
-from fastapi import APIRouter, requests
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/register")
+def register_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="register.html"
+    )
