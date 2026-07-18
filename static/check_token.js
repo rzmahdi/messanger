490604bb153c80e57f88d63caf7fa08484b3c3df
MahdiRz <mahdirazmkhah45@gmail.com>
2026-07-18 11:53:50 +0330

    feat(check_token): implement refreshAccessToken function for token renewal

--- static/check_token.js
+++ static/check_token.js
@@ -5,3 +5,32 @@ function isTokenExpiringSoon(token, buffer_seconds=60){
     const now = Math.floor(Date.now() / 1000);
     return payload.exp - now < buffer_seconds;
 }
+
+async function refreshAccessToken(){
+    const refresh_token = localStorage.getItem("refresh_token");
+    if(!refresh_token) return null;
+
+    try{
+        const res = await fetch("/refresh", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "refresh_token": refresh_token
+            })
+        })
+        if(!res.ok) throw new Error("Refresh failed");
+
+        const data = await res.json();
+        localStorage.setItem("access_token", data.access_token);
+        return data.access_token;
+    }
+    catch(e){
+        console.error("Token refresh Failed!", e);
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        window.location.href = "/login";
+        return null;
+    }
+}
